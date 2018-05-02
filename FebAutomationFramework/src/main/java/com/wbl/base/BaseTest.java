3f7a9f995daad339e6894108ea4b0745370b0d4d
WhiteboxHub <training@whitebox-learning.com>
2018-05-02 16:50:12 -0700

    framework

---
+++ FebAutomationFramework/src/main/java/com/wbl/base/BaseTest.java
@@ -0,0 +1,22 @@
+package com.wbl.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class BaseTest {
+	protected WebDriver driver;
+
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\resources\\newChrome\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("http://whiteboxqa.com/");
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.close();
+	}
+}
